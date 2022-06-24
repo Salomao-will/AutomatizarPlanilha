15eb7cda3d93351e4410596d4dddd54c4363703a
MatheusHFSantos <hfsantos.matheus@gmail.com>
2022-06-24 15:06:27 -0300

    update (lib pandas + PyEx.xlsx + pip install openpyxl)

--- index.py
+++ index.py
@@ -1,2 +1,7 @@
-print('Hello World')
-print("salve salve galerinha")
+import pandas as pd
+
+print("Hello world")
+
+x = pd.read_excel(r"C:\Users\mathe\AutomatizarPlanilha\excel\PyEx.xlsx", engine='openpyxl')
+
+print(x)
